915934e13dbb3d121758809c867bb8644ad7e40a
Marcus Lee <marcustutorial@hotmail.com>
2021-11-28 17:43:29 +0800

    used CDN for video

--- src/features/intro/components/VideoBG.tsx
+++ src/features/intro/components/VideoBG.tsx
@@ -11,7 +11,10 @@ export const VideoBG: React.FC = ({ children }) => {
         controls={false}
         className="w-[100vw] h-[100vh] object-center object-cover z-1"
       >
-        <source src="/src/assets/video/loopBGwithoutLogo.mp4" type="video/mp4" />
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/yw-combined-game.appspot.com/o/loopBGwithoutLogo.mp4?alt=media&token=8e204358-6ed5-4b6e-aa88-9d4685dfe378"
+          type="video/mp4"
+        />
       </video>
       {children}
     </div>
